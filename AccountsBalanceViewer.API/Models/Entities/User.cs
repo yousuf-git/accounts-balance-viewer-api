580250a995b7d116d8c4554b3b9143b311f62aa5
yousuf <mohammed.yousuf@pickme.lk>
2022-08-20 20:34:49 +0530

    add role to user model

--- AccountsBalanceViewer.API/Models/Entities/User.cs
+++ AccountsBalanceViewer.API/Models/Entities/User.cs
@@ -6,11 +6,14 @@ namespace AccountsViewer.API.Models.Entities;
 [Index(nameof(Email))]
 public class User
 {
+    public static string ROLE_ADMIN = "admin";
+    public static string ROLE_USER = "user";
+    
     public long Id { get; set; }
     public string Username { get; set; } = "";
     public string Name { get; set; } = "";
     public string Email { get; set; } = "";
     public string Password { get; set; } = "";
-
+    public string Role { get; set; } = "";
     public List<Entry> Entries { get; set; } = new();
 }
