abd007470fa19fffc842109a58d6e828efb6a999
yousuf <mohammed.yousuf@pickme.lk>
2022-08-21 18:17:12 +0530

    add user roles entity

---
+++ AccountsBalanceViewer.API/Models/Entities/UserRoles.cs
@@ -0,0 +1,8 @@
+namespace AccountsViewer.API.Models.Entities;
+
+public static class UserRoles
+{
+    public const string Admin = "admin";
+    public const string User = "user";
+    public const string AdminOrUser = "admin,user";
+}
